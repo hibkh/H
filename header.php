0b8ea9952ce0dd0a46fa36e247c6f9bf3fabbeff
hibkh <62379253+hibkh@users.noreply.github.com>
2020-09-29 11:24:28 +0300

    Initialize feature/web-app-manifest

---
+++ header.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying the header.
+ *
+ * @package TenUpScaffold
+ */
+
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="theme-color" content="#d23226" />
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
+		<?php wp_body_open(); ?>
+		<h1><?php bloginfo( 'name' ); ?></h1>
